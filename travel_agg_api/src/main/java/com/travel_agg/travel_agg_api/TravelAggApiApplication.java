c52fb44076b5839aa761af5b830c77260b8c6745
KHANHTQ-CTO\tranq <tranquockhanh307@gmail.com>
2024-04-02 10:51:54 +0700

    create project BE

---
+++ travel_agg_api/src/main/java/com/travel_agg/travel_agg_api/TravelAggApiApplication.java
@@ -0,0 +1,12 @@
+package com.travel_agg.travel_agg_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelAggApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(TravelAggApiApplication.class, args);
+	}
+
+}
---
+++ travel_agg_api/src/test/java/com/travel_agg/travel_agg_api/TravelAggApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travel_agg.travel_agg_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelAggApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
