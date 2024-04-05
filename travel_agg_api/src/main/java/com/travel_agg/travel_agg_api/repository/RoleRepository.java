fe66aee3c4381e57df0a45794cf04c182fe1bbb3
Trần Quốc Khánh <74222381+khanh307@users.noreply.github.com>
2024-04-05 08:14:21 +0700

    Revert "user api"

--- travel_agg_api/src/main/java/com/travel_agg/travel_agg_api/repository/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.travel_agg.travel_agg_api.repository;
-
-import com.travel_agg.travel_agg_api.entity.RoleEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface RoleRepository extends JpaRepository<RoleEntity, UUID> {
-}
